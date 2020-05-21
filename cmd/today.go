bcb54df1a1e88be5152fc573728a07d8eaf365a1
Arun Mathew <arunvmathew95@gmail.com>
2020-05-21 15:39:09 +0530

    Added command to fetch today's tasks from todoist

---
+++ cmd/today.go
@@ -0,0 +1,55 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/arunvm/mind/config"
+	"github.com/arunvm/mind/todoist"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+// todayCmd represents the today command
+var todayCmd = &cobra.Command{
+	Use:   "today",
+	Short: "A brief description of your command",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cfg, err := config.ReadConfigFile()
+		if err != nil {
+			log.Printf("Error when reading config file \n %v", err)
+			return err
+		}
+
+		tasks, err := todoist.GetTasks(cfg.TodoistToken, time.Now().Format("2006-01-02"))
+		if err != nil {
+			log.Printf("Error when fetching tasks \n %v", err)
+			return err
+		}
+
+		for _, task := range *tasks {
+			fmt.Println(task.Content, task.Due.Date)
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	todoistCmd.AddCommand(todayCmd)
+}
--- todoist/todoist.go
+++ todoist/todoist.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"net/http"
+	"time"
 )
 
 const (
@@ -12,6 +13,16 @@ const (
 
 type CreateTaskBody struct {
 	Content string `json:"content"`
+}
+
+type Task struct {
+	Content string `json:"content"`
+	Due     *Due   `json:"due"`
+}
+
+type Due struct {
+	Date   string `json:"date"`
+	String string `json:"string"`
 }
 
 func CreateTask(token, content string) error {
@@ -40,4 +51,57 @@ func CreateTask(token, content string) error {
 	defer resp.Body.Close()
 
 	return nil
+}
+
+func GetTasks(token, date string) (*[]Task, error) {
+	url := endpoint + "tasks"
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var tasks []Task
+	err = json.NewDecoder(resp.Body).Decode(&tasks)
+	if err != nil {
+		return nil, err
+	}
+
+	filteredTasks, err := filterTasksByDate(&tasks, date)
+	if err != nil {
+		return nil, err
+	}
+
+	return filteredTasks, nil
+}
+
+func filterTasksByDate(tasks *[]Task, date string) (*[]Task, error) {
+	var filteredTasks []Task
+
+	givenDate, err := time.Parse("2006-01-02", date)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, task := range *tasks {
+		if task.Due != nil {
+			taskDate, err := time.Parse("2006-01-02", task.Due.Date)
+			if err != nil {
+				return nil, err
+			}
+
+			if taskDate.Equal(givenDate) {
+				filteredTasks = append(filteredTasks, task)
+			}
+		}
+	}
+
+	return &filteredTasks, nil
 }
