4332b66d406728da974c11de676e6b74aad681b0
Arun Mathew <arunvmathew95@gmail.com>
2020-05-22 10:28:45 +0530

    Error logs are written into a file instead of standard output

--- main.go
+++ main.go
@@ -16,11 +16,29 @@ limitations under the License.
 package main
 
 import (
+	"os"
+	"path"
+
 	"github.com/arunvm/mind/cmd"
+	"github.com/mitchellh/go-homedir"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	// Find home directory.
+	home, err := homedir.Dir()
+	if err != nil {
+		log.Fatalf("Error when fetching home directory\n%v", err)
+	}
+
+	file, err := os.OpenFile(path.Join(home, ".mind.error.log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalf("Error when opening/creating error log file \n %v", err)
+	}
+	defer file.Close()
+
+	log.SetOutput(file)
+
 	log.SetFormatter(&log.JSONFormatter{})
 
 	cmd.Execute()
